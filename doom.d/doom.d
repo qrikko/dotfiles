42ec2c70e295d8077c0643c81c9aa9b9529c4be9
Qrikko <qrikko@gmail.com>
2023-06-19 16:30:00 +0200

    added emacs.d and doom.d to dotfiles

---
+++ doom.d/doom.d
@@ -0,0 +1,1 @@
+/home/qrikko/.dotfiles/dotfiles/doom.d
--- setup.sh
+++ setup.sh
@@ -7,5 +7,7 @@ ln -sf "${dotdir}/bspwm" $confdir/
 ln -sf "${dotdir}/sxhkd" $confdir/
 ln -sf "${dotdir}/polybar" $confdir/
 ln -sf "${dotdir}/tmux.conf" ~/.tmux.conf
+ln -sf "${dotdir}/doom.d" ~/.doom.d
+ln -sf "${dotdir}/emacs.d" ~/.emacs.d
 
 bspc wm -r
