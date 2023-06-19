dbe3ca558d73bf4d032a63fda717f063d2373fd5
Qrikko <qrikko@gmail.com>
2023-06-19 13:36:41 +0200

    added tmux.conf

--- setup.sh
+++ setup.sh
@@ -6,5 +6,6 @@ ln -sf "${dotdir}/picom.conf" $confdir/
 ln -sf "${dotdir}/bspwm" $confdir/
 ln -sf "${dotdir}/sxhkd" $confdir/
 ln -sf "${dotdir}/polybar" $confdir/
+ln -sf "${dotdir}/tmux.conf" ~/.tmux.conf
 
 bspc wm -r
