3a28be8e1f88d2cd50e21f12b2e880c9aab9f726
Qrikko aspire 5 <qrikko@gmail.com>
2023-06-21 13:04:29 +0200

    made some changes to setup-script

--- setup.sh
+++ setup.sh
@@ -1,20 +1,22 @@
 #!/usr/bin/env zsh
 
-#ln -sf ./zshrc ~/.zshrc
-cp -f ./zshrc ~/.zshrc
+#cp -f ./zshrc ~/.zshrc
+ln -sf ~/dotfiles/zshrc ~/.zshrc
 source ~/.zshrc
+
+sudo pacman -S sxhkd bspwm picom polybar
 
 confdir=~/.config
 
-cp -rf ./picom.conf $confdir/ 
-cp -rf  ./bspwm $confdir/
-cp -rf  ./sxhkd $confdir/
-cp -rf  ./polybar $confdir/
-cp -rf  ./doom.d ~/.doom.d
-cp -rf  ./emacs.d ~/.emacs.d
-cp -f  ./tmux.conf ~/.tmux.conf
+#cp -rf ./picom.conf $confdir/ 
+#cp -rf  ./bspwm $confdir/
+#cp -rf  ./sxhkd $confdir/
+#cp -rf  ./polybar $confdir/
+#cp -rf  ./doom.d ~/.doom.d
+#cp -rf  ./emacs.d ~/.emacs.d
+#cp -f  ./tmux.conf ~/.tmux.conf
 
-#ln -sf ./picom.conf $confdir/ 
+ln -sf ./picom.conf $confdir/ picom.conf
 #ln -sf ./bspwm $confdir/
 #ln -sf ./sxhkd $confdir/
 #ln -sf ./polybar $confdir/
@@ -22,4 +24,4 @@ cp -f  ./tmux.conf ~/.tmux.conf
 #ln -sf ./doom.d ~/.doom.d
 #ln -sf ./emacs.d ~/.emacs.d
 
-bspc wm -r
+#bspc wm -r
