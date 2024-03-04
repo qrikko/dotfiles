044eb7188916f6e7ed2b4772860c87f62359eb0e
Qrikko aspire 5 <qrikko@gmail.com>
2024-03-04 11:12:13 +0100

    work on a more dynamic setup script

--- setup.sh
+++ setup.sh
@@ -4,17 +4,32 @@
 ln -sf ~/dotfiles/zshrc ~/.zshrc
 source ~/.zshrc
 
-sudo pacman -S sxhkd bspwm picom polybar
-
 confdir=~/.config
+echo -n "Install (super-user needed!)  [s]xhkd, [b]spwm, [p]icom, poly[b]ar, [d]oom-emacs,  [a]ll or [n]one? [s,b,p,b, a/n]: "
+install=""
+read input
+len=${#input}
 
-#cp -rf ./picom.conf $confdir/ 
-#cp -rf  ./bspwm $confdir/
-#cp -rf  ./sxhkd $confdir/
-#cp -rf  ./polybar $confdir/
-#cp -rf  ./doom.d ~/.doom.d
-#cp -rf  ./emacs.d ~/.emacs.d
-#cp -f  ./tmux.conf ~/.tmux.conf
+for ((i=0; i<$len; i++)); do
+	#char="${input:i:1}"
+	char=$(echo "$input" | cut -c $((i+1)))
+
+	[[ $char == 's' ]] && install+="sxhkd " 
+	[[ $char == 'b' ]] && install+="bspwm "
+	[[ $char == 'p' ]] && install+="picom "
+	[[ $char == 'b' ]] && install+="polybar "
+	[[ $char == 'd' ]] && install+="emacs " && install_doom=true
+	[[ $char == 'a' ]] && install="sxhkd bspwm picom polybar emacs" && install_doom=true
+done
+
+if [[ -n $install ]]; then
+	sudo pacman -S $install
+fi
+
+if [[ $install_doom == true ]]; then
+	git clone --depth 1 --single-branch https://github.com/doomemacs/doomemacs ~/.config/emacs 
+	~/.config/emacs/bin/doom install
+fi
 
 ln -sf ./picom.conf $confdir/ picom.conf
 #ln -sf ./bspwm $confdir/
